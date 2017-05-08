5a7894c30950ed4b285748496fbce3f350ffdfa9
HesselM <hmolen.science@gmail.com>
2017-05-08 12:22:40 +0200

    added buffer for testing

--- FlashCam_test_PLL.cpp
+++ FlashCam_test_PLL.cpp
@@ -50,8 +50,15 @@ static cv::Mat U;
 static cv::Mat V;
 
 static double prev;
-static int  frames;
 static double sum;
+
+//NOTE: width/height are rounded/aligned in FlashCam-lib! For actual values, check setting-values after initialisation
+#define FRAME_WIDTH  640
+#define FRAME_HEIGHT 480
+#define MAX_FRAMES   1000
+static int frames;
+
+static unsigned char framebuffer[MAX_FRAMES][FRAME_WIDTH*FRAME_HEIGHT]
 
 void flashcam_callback(unsigned char *frame, int w, int h) {
     //fprintf(stdout, "callback: %p (%d x %d)\n", frame, w, h);
@@ -59,9 +66,13 @@ void flashcam_callback(unsigned char *frame, int w, int h) {
     //Y.data = (uchar*) &(frame[0]);
     //U.data = (uchar*) &(frame[(w*h*1)>>0]);
     //V.data = (uchar*) &(frame[(w*h*5)>>2]);
-    memcpy(Y.data, &(frame[0])         , w*h);
-    memcpy(U.data, &(frame[(w*h*1)>>0]), (w>>1)*(h>>1));
-    memcpy(V.data, &(frame[(w*h*5)>>2]), (w>>1)*(h>>1));
+    
+    //memcpy(Y.data, &(frame[0])         , w*h);
+    //memcpy(U.data, &(frame[(w*h*1)>>0]), (w>>1)*(h>>1));
+    //memcpy(V.data, &(frame[(w*h*5)>>2]), (w>>1)*(h>>1));
+    
+    //copy data to framebuffer
+    memcpy(&(framebuffer[frames][0]), &(frame[0]), w*h);
     
     //timing
     double tick    = cv::getTickCount();
@@ -74,7 +85,6 @@ void flashcam_callback(unsigned char *frame, int w, int h) {
         fprintf(stdout, "Timing: %f (avg: %f; frames: %d; fps:%f)\n", elapsed, sum/frames, frames, 1.0/(sum/frames));
 }
 
-
 int main(int argc, const char **argv) {
     fprintf(stdout, "\n -- VIDEO-TEST: PLL -- \n\n");
     
@@ -85,8 +95,8 @@ int main(int argc, const char **argv) {
     FLASHCAM_PARAMS_T params = {};
     FlashCam::getDefaultParams( &params );
     //update params
-    settings.width=640;
-    settings.height=480;
+    settings.width=FRAME_WIDTH;
+    settings.height=FRAME_HEIGHT;
     settings.verbose=1;
     settings.update=0;
     settings.mode=FLASHCAM_MODE_VIDEO;
@@ -105,15 +115,17 @@ int main(int argc, const char **argv) {
     //camera.setISO(800);
     FlashCam::get().setRotation(270);
     
-    float        pll_fps = 10;
+    float        pll_fps = 120
+    ;
     //float pll_pw  = 500.0f / (pll_fps / pll_div); //50% dutycycle
     //float pll_pw  = 33.333333; // ms
-    float        pll_pw  = 3000/pll_fps; // ms
+    float        pll_pw  = 3000/pll_fps; // ms -> 1/3 of fps
     unsigned int pll_div = 1;
-    unsigned int pll_offset = 1000;      // us
+    unsigned int pll_offset = 0;      // us
     
     FlashCam::get().setFrameRate(pll_fps);
-
+    FlashCam::get().setShutterSpeed(pll_pw/1000); //same as pll
+    
     //enable PLL
     FlashCam::get().setPLLEnabled(1);
     FlashCam::get().setPLLDivider(pll_div);
@@ -130,7 +142,7 @@ int main(int argc, const char **argv) {
     
     //create openCV window
     Y.create( settings.height, settings.width, CV_8UC1 );
-    cv::namedWindow( "Y", cv::WINDOW_AUTOSIZE );
+    //cv::namedWindow( "Y", cv::WINDOW_AUTOSIZE );
     U.create( settings.height >> 1, settings.width >> 1, CV_8UC1 );
     //cv::namedWindow( "U", cv::WINDOW_AUTOSIZE );
     V.create( settings.height >> 1, settings.width >> 1, CV_8UC1 );
@@ -147,28 +159,64 @@ int main(int argc, const char **argv) {
     //wait
     int show =  0;  //show image with opencv?
     int time =  10;  //total time (seconds) of streaming/running
-    int fps  =  5;  //refreshrate of window
+    int fps  =  1;  //refreshrate of window
     
     //refresh loop
+    /*
     for (int i=0; i<(time*fps); i++) {
         if (show) {
             cv::imshow ("Y", Y);
             cv::imshow ("U", U);
             cv::imshow ("V", V);
             cv::waitKey(1000/fps);
-        } else
-            usleep(1000000/fps);
+        } else {
+            if (frames > 0) {
+
+                char filename[11];
+                sprintf(filename, "%010d.png", frames);
+                fprintf(stdout, "%s\n", filename);
+                
+                std::vector<int> compression_params;
+                compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+                compression_params.push_back(9);
+                
+                cv::imwrite(filename, Y, compression_params);
+
+            } else {
+                usleep(1000000/fps);
+            }
+        }
     }
+    */
     
+    //wait for framebuffer to be filled
+    while (frames < MAX_FRAMES)
+        usleep(1000000); //1sec sleep
+
     //start stream image
     FlashCam::get().stopCapture();   
+
+    //write to files
+    for (int i=0l i<MAX_FRAMES; i++) {
+
+        //set data
+        Y.data = &(framebuffer[i][0]);
+
+        //create filename
+        char filename[11];
+        sprintf(filename, "%010d.jpg", frames);
+        fprintf(stdout, "%s\n", filename);
+        
+        //write to file
+        cv::imwrite(filename, Y);
+    }
     
     //show params
-    FlashCam::get().getParams( &params , true);   
-    FlashCam::printParams( &params ); 
+    //FlashCam::get().getParams( &params , true);   
+    //FlashCam::printParams( &params ); 
     //show settings
-    FlashCam::get().getSettings( &settings);   
-    FlashCam::printSettings( &settings ); 
+    //FlashCam::get().getSettings( &settings);   
+    //FlashCam::printSettings( &settings ); 
 
     return 0;
 }
